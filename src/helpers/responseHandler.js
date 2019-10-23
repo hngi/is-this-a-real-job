9a4b2ace75fae99c78db7d207fbe1438d07a883b
LeanKhan <eslean20@gmail.com>
2019-10-23 19:24:39 +0100

    feat: add token field to response

--- src/helpers/responseHandler.js
+++ src/helpers/responseHandler.js
@@ -5,24 +5,41 @@
  */
 
 /**
-  * @param  {Object} res
-  * @param  {Number} statusCode
-  * @param  {String} message
-  * @param {Object} additionalFields
-  * @returns {Object} null
-  */
-export const respondWithSuccess = (res, statusCode = 200, message, additionalFields = {}) => {
-  const payload = Array.isArray(additionalFields) ? [ ...additionalFields ] : { ...additionalFields}
-  
-  return res.status(statusCode).send({ success: true, message, payload });
-} 
- 
+ * @param  {Object} res
+ * @param  {Number} statusCode
+ * @param  {String} message
+ * @param {Object} additionalFields
+ * @returns {Object} null
+ */
+export const respondWithSuccess = (
+  res,
+  statusCode = 200,
+  message,
+  additionalFields = {},
+  token = ""
+) => {
+  const payload = Array.isArray(additionalFields)
+    ? [...additionalFields]
+    : { ...additionalFields };
+
+  return res
+    .status(statusCode)
+    .send({ success: true, message, payload, token });
+};
+
 /**
-  * @param  {Object} res
-  * @param  {Number} statusCode
-  * @param  {String} message
-  * @param {Object} additionalFields
-  * @returns {Object} null
-  */
-export const respondWithWarning = (res, statusCode = 500, message, additionalFields = {}) => res
-  .status(statusCode).send({ success: false, message, payload: { ...additionalFields } });
+ * @param  {Object} res
+ * @param  {Number} statusCode
+ * @param  {String} message
+ * @param {Object} additionalFields
+ * @returns {Object} null
+ */
+export const respondWithWarning = (
+  res,
+  statusCode = 500,
+  message,
+  additionalFields = {}
+) =>
+  res
+    .status(statusCode)
+    .send({ success: false, message, payload: { ...additionalFields } });
