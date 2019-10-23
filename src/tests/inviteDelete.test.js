d8eae312235a34fdecf80aef9248bfe6736879d7
Abdulhafiz Ahmed <abdulhafizahmed01@gmail.com>
2019-10-23 22:33:16 +0700

    delete Invite test

---
+++ src/tests/inviteDelete.test.js
@@ -0,0 +1,29 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../index';
+
+const { expect } = chai;
+chai.use(chaiHttp);
+
+const deleteUrl = '/api/v1/invite/inviteId';
+
+describe('DELETE INVITE', () => {
+  it('it should delete a post from the database if request is sent by an admin', (done) => {
+    chai.request(app)
+      .delete(`${deleteUrl}/1ca46a2e-61bc-4c51-8355-660ba808cae5`)
+      .end((error, res) => {
+        expect(res).to.have.status(204);
+        expect(res).to.be.a('object');
+        done();
+      });
+  });
+  it('it should return 404 on resource not found', (done) => {
+    chai.request(app)
+      .patch(`${deleteUrl}/MaLf90rMeD_iD`)
+      .end((error, res) => {
+        expect(res).to.have.status(404);
+        expect(res.body.message).to.equal('Not Found');
+        done();
+      });
+  });
+});
