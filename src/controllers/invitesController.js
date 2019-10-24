10c4f265cd0728537aecca128dfa40324ca09610
Akinlade Feranmi <feranmiakinlade@gmail.com>
2019-10-24 09:37:44 +0100

    Almost done now...

--- src/controllers/invitesController.js
+++ src/controllers/invitesController.js
@@ -6,25 +6,44 @@ import {
 import { fetchOneInvite, fetchAllInvites, saveInvite } from '../services/inviteServices';
 
 export const getOneInvite = async (req, res)=> {
-  const { inviteId } = req.params;
+  try {
+    const { inviteId } = req.params;
 
-  const invite = await fetchOneInvite({inviteId});
+    const invite = await fetchOneInvite({inviteId});
 
-  if (invite)
-    return respondWithSuccess(res, 200, 'Invite found', invite);
-  else
-    return respondWithWarning(res, 404, 'Invite not found');
+    if (invite)
+      return respondWithSuccess(res, 200, 'Invite found', invite);
+    else
+      return respondWithWarning(res, 404, 'Invite not found');
+  }
+  catch (error) {
+    return respondWithWarning(res, 500, 'Server error');
+  }
 };
 
 export const getAllInvites = async (req, res)=> {
-  const invitesList = await fetchAllInvites();
+  try {
+    const invitesList = await fetchAllInvites();
 
-  if (invitesList.length > 0)
-    return respondWithSuccess(res, 200, 'Retrieved invites', invitesList);
-  else
-    return respondWithWarning(res, 404, 'Failed to fetch invites');
+    if (invitesList.length > 0)
+      return respondWithSuccess(res, 200, 'Retrieved invites', invitesList);
+    else
+      return respondWithWarning(res, 404, 'Failed to fetch invites');
+  }
+  catch (error) {
+    return respondWithWarning(res, 500, 'Server error');
+  }
 };
 
 export const saveNewInvite = async (req, res)=> {
-  saveInvite(req.body);
+  try {
+    const { body, userId } = req.body;
+
+    const invite = await saveInvite({ body, userId }).catch(error => { throw error; });
+    
+    return respondWithSuccess(res, 200, 'Comment added successfully', invite);
+  } 
+  catch (error) {
+    return respondWithWarning(res, error.status, error.message);
+  }
 };
