eb61cfc3292903b7027b6656660ced6442ee0eb4
Ennyola <medunoyeeni@gmail.com>
2019-10-24 10:27:16 +0100

    Job invite page

---
+++ src/routes/jobInvitesRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const jobInviteRouter = express.Router();
+
+jobInviteRouter.route('/')
+    .get((req, res) => {
+        res.render('job invites', {}
+
+
+        )
+    })
+
+module.exports = jobInviteRouter;
