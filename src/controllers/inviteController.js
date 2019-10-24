7b15c2cb3339abea1ab270aa6f417cd6b8cb904c
shonubijerry <shonubijerry@gmail.com>
2019-10-24 22:15:11 +0100

    Lint code

--- src/controllers/inviteController.js
+++ src/controllers/inviteController.js
@@ -1,5 +1,5 @@
 import { respondWithSuccess, respondWithWarning } from '../helpers/responseHandler';
-import { 
+import {
   deleteOneInvite,
   upvoteOneInvite,
   fetchOneInvite,
@@ -7,43 +7,36 @@ import {
   saveInvite
 } from '../services/inviteServices';
 
-export const getOneInvite = async (req, res)=> {
+export const getOneInvite = async (req, res) => {
   try {
     const { inviteId } = req.params;
 
-    const invite = await fetchOneInvite({inviteId});
+    const invite = await fetchOneInvite({ inviteId });
 
-    if (invite)
-      return respondWithSuccess(res, 200, 'Invite found', invite);
-    else
-      return respondWithWarning(res, 404, 'Invite not found');
-  }
-  catch (error) {
-    //console.log(error);
-    return respondWithWarning(res, 500, 'Server error');
+    if (invite) { return respondWithSuccess(res, 200, 'Invite found', invite); }
+    respondWithWarning(res, 404, 'Invite not found');
+  } catch (error) {
+    respondWithWarning(res, 500, 'Server error');
   }
 };
 
-export const getAllInvites = async (req, res)=> {
+export const getAllInvites = async (req, res) => {
   try {
     const invitesList = await fetchAllInvites();
 
-    return respondWithSuccess(res, 200, 'Retrieved invites', invitesList);
-  }
-  catch (error) {
-    //console.log(error);
-    return respondWithWarning(res, 500, 'Server error');
+    respondWithSuccess(res, 200, 'Retrieved invites', invitesList);
+  } catch (error) {
+    respondWithWarning(res, 500, 'Server error');
   }
 };
 
-export const saveNewInvite = async (req, res)=> {
+export const saveNewInvite = async (req, res) => {
   try {
     const invite = await saveInvite(req.body).catch(error => { throw error; });
-    
-    return respondWithSuccess(res, 201, 'Job Invite submitted successfully', invite);
-  } 
-  catch (error) {
-    return respondWithWarning(res, error.status, error.message);
+
+    respondWithSuccess(res, 201, 'Job Invite submitted successfully', invite);
+  } catch (error) {
+    respondWithWarning(res, error.status, error.message);
   }
 };
 
@@ -56,10 +49,10 @@ export const saveNewInvite = async (req, res)=> {
 export const deleteInvite = async (req, res) => {
   const { inviteId, title } = req.invite;
   if (!inviteId) {
-    return respondWithWarning(res, 400, 'Bad Request');
+    respondWithWarning(res, 400, 'Bad Request');
   }
   await deleteOneInvite({ inviteId });
-  return respondWithSuccess(res, 200, `${title} deleted successfully`);
+  respondWithSuccess(res, 200, `${title} deleted successfully`);
 };
 
 /**
@@ -74,5 +67,5 @@ export const upvoteInvite = async (req, res) => {
   // user vote will determine if upvote or downvote
   const vote = voteType === 'true' ? upVotes + 1 : upVotes - 1;
   const invite = await upvoteOneInvite(vote, { inviteId });
-  return respondWithSuccess(res, 200, 'Upvote successful', invite.toJSON());
+  respondWithSuccess(res, 200, 'Upvote successful', invite.toJSON());
 };
