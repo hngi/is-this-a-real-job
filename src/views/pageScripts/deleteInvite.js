19ff1c7d38db3729d6e402d5b3267cf5599fd8de
Abdulhafiz Ahmed <abdulhafizahmed01@gmail.com>
2019-10-26 11:04:48 +0700

    API deleteInvite code fix with inviteId

--- src/views/pageScripts/deleteInvite.js
+++ src/views/pageScripts/deleteInvite.js
@@ -1,31 +1,28 @@
+/* eslint-disable no-loop-func */
+/* eslint-disable no-plusplus */
 /* eslint-disable no-console */
 /* eslint-disable no-undef */
-function togglePreloader(state) {
-  const preloader = document.querySelector('#cover');
-  preloader.style.display = state;
-}
 
-const deleteBtn = document.querySelector('#delete-btn');
-
+const api = new ItarjApi('/api/v1');
 const notification = document.querySelector('.notification');
-// This is to be completed when POST fetch API is implemented to deliver inviteId
-// const inviteId = document.querySelector('.inviteId').innerText;
-const inviteId = 'fdbd468a-2976-4f2f-8fe0-ae12dd8d5101'; // This deletes a real Invite inside the seeded DB
-deleteBtn.addEventListener('click', () => {
-  const api = new ItarjApi('/api/v1');
-  console.log('delete clicked');
+const deleteBtns = [...document.querySelectorAll('#delete-btn')];
+const inviteIds = [...document.querySelectorAll("input[type='hidden']")];
 
-  api.Delete(`invites/${inviteId}`, true)
-    .then(res => {
-      console.log(res);
-      window.location.href = '/admin/posts';
-    })
-    .catch(error => {
-      console.log(error);
-      notification.innerHTML = error.data.message;
-      notification.className += ' show';
-      setTimeout(() => {
-        notification.className = 'notification';
-      }, 5000);
-    });
-});
+const btns = deleteBtns.map(deleteBtn => deleteBtn);
+const invites = inviteIds.map(invite => invite.value);
+
+for (let i = 0; i < deleteBtns.length; i++) {
+  btns[i].addEventListener('click', () => {
+    api.Delete(`invites/${invites[i]}`, true)
+      .then(res => {
+        window.location.href = '/admin/posts';
+      })
+      .catch(error => {
+        notification.innerHTML = error.data.message;
+        notification.className += ' show';
+        setTimeout(() => {
+          notification.className = 'notification';
+        }, 5000);
+      });
+  });
+}
