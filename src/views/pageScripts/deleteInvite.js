ef5e00324a13c099d6dedd6bcc8d0e12f15bca4f
Abdulhafiz Ahmed <abdulhafizahmed01@gmail.com>
2019-10-26 10:00:40 +0700

    Consume API deleteInvite code fix

--- src/views/pageScripts/deleteInvite.js
+++ src/views/pageScripts/deleteInvite.js
@@ -1,14 +1,24 @@
 /* eslint-disable no-console */
 /* eslint-disable no-undef */
+function togglePreloader(state) {
+  const preloader = document.querySelector('#cover');
+  preloader.style.display = state;
+}
+
 const deleteBtn = document.querySelector('#delete-btn');
+
 const notification = document.querySelector('.notification');
+// This is to be completed when POST fetch API is implemented to deliver inviteId
+// const inviteId = document.querySelector('.inviteId').innerText;
+const inviteId = 'fdbd468a-2976-4f2f-8fe0-ae12dd8d5101'; // This deletes a real Invite inside the seeded DB
 deleteBtn.addEventListener('click', () => {
   const api = new ItarjApi('/api/v1');
+  console.log('delete clicked');
 
-  api.Post(`invites/${inviteId}`, true)
+  api.Delete(`invites/${inviteId}`, true)
     .then(res => {
       console.log(res);
-      window.location.href = '/admin';
+      window.location.href = '/admin/posts';
     })
     .catch(error => {
       console.log(error);
