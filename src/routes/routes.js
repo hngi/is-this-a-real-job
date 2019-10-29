1d8bed418d3da090d6ecc13abb98eea9a7306944
shonubijerry <shonubijerry@gmail.com>
2019-10-30 00:40:42 +0100

    fix test cases not passing

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,4 @@
-import { signin, signup } from "../controllers/authController";
+import { signin, signup } from '../controllers/authController';
 import {
   validateSigninFormData,
   validateSignupFormData,
@@ -15,7 +15,7 @@ import {
   validateUserId,
   validateUpvoteInput,
   validateInviteOwner
-} from "../middlewares/middlewares";
+} from '../middlewares/middlewares';
 
 import {
   deleteInvite,
@@ -48,32 +48,30 @@ export const initRoutes = app => {
   app.get('/login', (req, res) => res.render('login', { isAuth: false }));
   app.get('/register', (req, res) => res.render('register', { isAuth: false }));
   app.get('/post', (req, res) => res.render('userPost', { isAuth: true }));
-  app.get("/howitworks", (req, res) =>
-    res.render("howitworks", { isAuth: false })
-  );
+  app.get('/howitworks', (req, res) => res.render('howitworks', { isAuth: false }));
   app.get('/jobInvites', renderJobInvitesPage);
   app.get('/post/:inviteId', renderSinglePostPage);
   app.get('/admin/reported', (req, res) => res.render('admin/reportedUsers', { isAuth: true, }));
 
 
   // Edit post endpoint
-  app.get("/post/:inviteId/edit", validateInviteId, validateInvite, editInvite);
+  app.get('/post/:inviteId/edit', validateInviteId, validateInvite, editInvite);
 
-  app.get("/admin/users", renderAdminUsersPage);
-  app.get("/admin/posts", renderAdminJobInvitesPage);
+  app.get('/admin/users', renderAdminUsersPage);
+  app.get('/admin/posts', renderAdminJobInvitesPage);
 
   // All backend API endpoints below -----------------------------------------------------
   // Auth
-  app.post("/api/v1/auth/signin", validateSigninFormData, validUser, signin);
+  app.post('/api/v1/auth/signin', validateSigninFormData, validUser, signin);
   app.post(
-    "/api/v1/auth/signup",
+    '/api/v1/auth/signup',
     validateSignupFormData,
     verifyUniqueUser,
     signup
   );
 
   // Get all Users
-  app.get("/api/v1/users", authenticateUserToken, validateAdmin, getUsers);
+  app.get('/api/v1/users', authenticateUserToken, validateAdmin, getUsers);
 
   // Get single User - return JSON
   app.get('/api/v1/users/json/:username', getUser);
@@ -83,7 +81,7 @@ export const initRoutes = app => {
 
   // Block a user
   app.patch(
-    "/api/v1/users/block/:userId",
+    '/api/v1/users/block/:userId',
     validateUserId,
     authenticateUserToken,
     validateAdmin,
@@ -93,32 +91,32 @@ export const initRoutes = app => {
 
   // Post a new job invite.
   app.post(
-    "/api/v1/invites",
-    authenticateUserToken,
+    '/api/v1/invites',
     validateInviteData,
+    authenticateUserToken,
     saveNewInvite
   );
 
   // Get all job invites in the database.
-  app.get("/api/v1/invites", getAllInvites);
+  app.get('/api/v1/invites', getAllInvites);
 
   // Get a single job invite.
-  app.get("/api/v1/invites/:inviteId", validateInviteId, getOneInvite);
+  app.get('/api/v1/invites/:inviteId', validateInviteId, getOneInvite);
 
   // Update an existing job invite.
   app.put(
-    "/api/v1/invites/:inviteId",
-    authenticateUserToken,
+    '/api/v1/invites/:inviteId',
+    validateInviteUpdateData,
     validateInviteId,
+    authenticateUserToken,
     validateInvite,
     validateInviteOwner,
-    validateInviteUpdateData,
     updateInvite
   );
 
   // Delete an existing job invite.
   app.delete(
-    "/api/v1/invites/:inviteId",
+    '/api/v1/invites/:inviteId',
     validateInviteId,
     authenticateUserToken,
     validateAdmin,
@@ -127,20 +125,21 @@ export const initRoutes = app => {
   );
 
   // Get all comments for a given Invite.
-  app.get("/api/v1/comments/:inviteId", validateInviteId, getComments);
+  app.get('/api/v1/comments/:inviteId', validateInviteId, getComments);
 
   // Post a comment on a specific Invite.
   app.post(
-    "/api/v1/comments/:inviteId",
+    '/api/v1/comments/:inviteId',
+    validateCommentData,
     validateInviteId,
     authenticateUserToken,
-    validateCommentData,
+    validateInvite,
     createComment
   );
 
   // Upvote/Downvote a specific Invite.
   app.patch(
-    "/api/v1/invites/upvote/:inviteId/:voteType",
+    '/api/v1/invites/upvote/:inviteId/:voteType',
     validateUpvoteInput,
     validateInvite,
     upvoteInvite
@@ -151,7 +150,5 @@ export const initRoutes = app => {
   app.post('/api/v1/notifications', validateNotificationData, createNotification);
 
   // Fallback case for unknown URIs.
-  app.all("*", (req, res) =>
-    res.status(404).json({ message: "Route Not Found" })
-  );
+  app.all('*', (req, res) => res.status(404).json({ message: 'Route Not Found' }));
 };
