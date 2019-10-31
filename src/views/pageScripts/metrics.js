51034f9ce6fed657e0d2c1ae14dd392e3377df74
Endowmissy <adeolaayo31@yahoo.com>
2019-10-31 17:23:44 +0100

    added the scripts that will enable the API to be consume by frontend

--- src/views/pageScripts/metrics.js
+++ src/views/pageScripts/metrics.js
@@ -1,13 +1,25 @@
 
-
-function togglePreloader(state) {
+if (document.querySelector('#no-of-users')) 
+ {function togglePreloader(state) {
     const preloader = document.querySelector('#cover');
     preloader.style.display = state;
   }
+
+  const api = new ItarjApi('/api/v1');
+  const noOfUsers = document.querySelector('#no-of-users');
+  const noOfComments = document.querySelector('#no-of-comments');
+  const noOfPosts = document.querySelector('#no-of-posts');
   
-  if (document.querySelector('.metrics-section')) {
-    const api = new ItarjApi('/api/v1');
-    const metrics = [...document.querySelectorAll('.block-btn')];
-    const userIds = [...document.querySelectorAll("input[type='hidden']")];
-    const notification = document.querySelector('.notification');
+
+  const getMetricsNumbers = () => {
+    api.Get(`metrics`)
+      .then(res => {
+        noOfUsers.innerHTML = res.data.usersCount;
+        noOfComments.innerHTML = res.data.commentsCount ;
+        noOfPosts.innerHTML = res.data.invitesCount;
+        console.log(res.data)
+      })
+      .catch(err => console.log(err));
+  };
+getMetricsNumbers();
   }
