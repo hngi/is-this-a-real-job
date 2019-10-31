1695bb4e8245b410c7d63738edea893451a513c2
TechieTee <dennis.okoede@gmail.com>
2019-10-31 17:15:27 +0100

    report user page by Dennis

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,4 @@
-import { signin, signup } from '../controllers/authController';
+import { signin, signup } from "../controllers/authController";
 import {
   validateSigninFormData,
   validateSignupFormData,
@@ -18,8 +18,8 @@ import {
   passportAuthenticate,
   multerUploads,
   verifyUniqueUserUsername,
-  verifyUniqueUserEmail,
-} from '../middlewares/middlewares';
+  verifyUniqueUserEmail
+} from "../middlewares/middlewares";
 
 import {
   deleteInvite,
@@ -36,10 +36,10 @@ import {
   upvoteInvite,
   downvoteInvite,
   unvoteInvite,
-  fetchVoteCount,
-} from '../controllers/inviteController';
+  fetchVoteCount
+} from "../controllers/inviteController";
 
-import { getComments, createComment } from '../controllers/commentController';
+import { getComments, createComment } from "../controllers/commentController";
 import {
   blockUser,
   getUsers,
@@ -48,11 +48,18 @@ import {
   renderUserProfile,
   getUserByUserId,
   renderAdminReportedUsersPage
-} from '../controllers/userController';
-import { getNotifications, createNotification } from '../controllers/notificationController';
-import { validateNotificationData } from '../middlewares/validateNotification';
-import { validateCookies, signUserIn, signUserOut } from '../middlewares/cookieHandler';
-import { getMetrics } from '../controllers/metricsController';
+} from "../controllers/userController";
+import {
+  getNotifications,
+  createNotification
+} from "../controllers/notificationController";
+import { validateNotificationData } from "../middlewares/validateNotification";
+import {
+  validateCookies,
+  signUserIn,
+  signUserOut
+} from "../middlewares/cookieHandler";
+import { getMetrics } from "../controllers/metricsController";
 
 export const initRoutes = app => {
   // Cookie handlers before all
@@ -62,62 +69,100 @@ export const initRoutes = app => {
 
   // All EJS frontend endpoints below --------------------------------------------------
 
-  app.get('/', (req, res) => res.render('index', { isAuth: req.isAuth, isAdmin: req.auth.isAdmin })); // Pass true or false to toggle state of navbar....
-  app.get('/login', (req, res) => res.render('login', { isAuth: req.isAuth, isAdmin: req.auth.isAdmin }));
-  app.get('/register', (req, res) => res.render('register', { isAuth: req.isAuth, isAdmin: req.auth.isAdmin }));
-  app.get('/post', getUserByUserId, (req, res) => res.render('userPost', {
-    isAuth: req.isAuth,
-    isAdmin: req.auth.isAdmin,
-    user: req.user,
-    username: req.auth.username,
-    name: req.auth.name,
-  }));
-  app.get('/howitworks', (req, res) => res.render('howitworks', { isAuth: req.isAuth, isAdmin: req.auth.isAdmin }));
-  app.get('/posts', renderJobInvitesPage);
-  app.get('/post/:inviteId', renderSinglePostPage);
-  app.get('/about', (req, res) => res.render('about', {
-    isAuth: req.isAuth, isAdmin: req.auth.isAdmin, username: req.auth.username, name: req.auth.name
-  }));
-  app.get('/admin/reported', (req, res) => res.render('admin/reportedUsers', { isAuth: req.auth.isAuth, isAdmin: req.auth.isAdmin }));
-  app.get('/reportUser', (req, res) => res.render('reportUser', {
-    isAuth: false, username: req.auth.username, name: req.auth.name, isAdmin: req.auth.isAdmin
-  }));
-  app.get('/users/:username', renderUserProfile);
-  app.get('/admin/reportedusers', renderAdminReportedUsersPage);
+  app.get("/", (req, res) =>
+    res.render("index", { isAuth: req.isAuth, isAdmin: req.auth.isAdmin })
+  ); // Pass true or false to toggle state of navbar....
+  app.get("/login", (req, res) =>
+    res.render("login", { isAuth: req.isAuth, isAdmin: req.auth.isAdmin })
+  );
+  app.get("/register", (req, res) =>
+    res.render("register", { isAuth: req.isAuth, isAdmin: req.auth.isAdmin })
+  );
+  app.get("/post", getUserByUserId, (req, res) =>
+    res.render("userPost", {
+      isAuth: req.isAuth,
+      isAdmin: req.auth.isAdmin,
+      user: req.user,
+      username: req.auth.username,
+      name: req.auth.name
+    })
+  );
+  app.get("/howitworks", (req, res) =>
+    res.render("howitworks", { isAuth: req.isAuth, isAdmin: req.auth.isAdmin })
+  );
+  app.get("/reportuser", (req, res) =>
+    res.render("reportuser", { isAuth: req.isAuth, isAdmin: req.auth.isAdmin })
+  );
+  app.get("/posts", renderJobInvitesPage);
+  app.get("/post/:inviteId", renderSinglePostPage);
+  app.get("/about", (req, res) =>
+    res.render("about", {
+      isAuth: req.isAuth,
+      isAdmin: req.auth.isAdmin,
+      username: req.auth.username,
+      name: req.auth.name
+    })
+  );
+  app.get("/admin/reported", (req, res) =>
+    res.render("admin/reportedUsers", {
+      isAuth: req.auth.isAuth,
+      isAdmin: req.auth.isAdmin
+    })
+  );
+  app.get("/reportUser", (req, res) =>
+    res.render("reportUser", {
+      isAuth: false,
+      username: req.auth.username,
+      name: req.auth.name,
+      isAdmin: req.auth.isAdmin
+    })
+  );
+  app.get("/users/:username", renderUserProfile);
+  app.get("/admin/reportedusers", renderAdminReportedUsersPage);
   // Search Invites - Renders view
-  app.get('/invites/search', renderSearchResults);
-  app.get('/admin', (req, res) => res.render('./admin/index', {
-    isAuth: false, username: req.auth.username, name: req.auth.name, isAdmin: req.auth.isAdmin
-  }));
-
+  app.get("/invites/search", renderSearchResults);
+  app.get("/admin", (req, res) =>
+    res.render("./admin/index", {
+      isAuth: false,
+      username: req.auth.username,
+      name: req.auth.name,
+      isAdmin: req.auth.isAdmin
+    })
+  );
 
   // Edit post endpoint
-  app.get('/post/:inviteId/edit', validateInviteId, validateInvite, getUserByUserId, renderEditInvitePage);
+  app.get(
+    "/post/:inviteId/edit",
+    validateInviteId,
+    validateInvite,
+    getUserByUserId,
+    renderEditInvitePage
+  );
 
-  app.get('/admin/users', renderAdminUsersPage);
-  app.get('/admin/posts', renderAdminJobInvitesPage);
+  app.get("/admin/users", renderAdminUsersPage);
+  app.get("/admin/posts", renderAdminJobInvitesPage);
 
   // All backend API endpoints below -----------------------------------------------------
   // Auth
-  app.post('/api/v1/auth/signin', validateSigninFormData, validUser, signin);
+  app.post("/api/v1/auth/signin", validateSigninFormData, validUser, signin);
   app.post(
-    '/api/v1/auth/signup',
+    "/api/v1/auth/signup",
     validateSignupFormData,
     verifyUniqueUserEmail,
     verifyUniqueUserUsername,
     signup
   );
   // Twitter Login
-  app.get('/auth/twitter', passportAuthenticate);
-  app.get('/auth/twitter/callback', passportAuthCallback);
+  app.get("/auth/twitter", passportAuthenticate);
+  app.get("/auth/twitter/callback", passportAuthCallback);
   // Get all Users
-  app.get('/api/v1/users', authenticateUserToken, validateAdmin, getUsers);
+  app.get("/api/v1/users", authenticateUserToken, validateAdmin, getUsers);
 
   // Get single User - return JSON
-  app.get('/api/v1/users/json/:username', getUser);
+  app.get("/api/v1/users/json/:username", getUser);
   // Block a user
   app.patch(
-    '/api/v1/users/block/:userId',
+    "/api/v1/users/block/:userId",
     validateUserId,
     authenticateUserToken,
     validateAdmin,
@@ -127,7 +172,7 @@ export const initRoutes = app => {
 
   // Post a new job invite.
   app.post(
-    '/api/v1/invites',
+    "/api/v1/invites",
     authenticateUserToken,
     multerUploads,
     validateInviteData,
@@ -135,17 +180,17 @@ export const initRoutes = app => {
   );
 
   // Get all job invites in the database.
-  app.get('/api/v1/invites', getAllInvites);
+  app.get("/api/v1/invites", getAllInvites);
 
   // Search Invites - Returns JSON payload
-  app.get('/api/v1/invites/search/json', searchInvitesApi);
+  app.get("/api/v1/invites/search/json", searchInvitesApi);
 
   // Get a single job invite.
-  app.get('/api/v1/invites/:inviteId', validateInviteId, getOneInvite);
+  app.get("/api/v1/invites/:inviteId", validateInviteId, getOneInvite);
 
   // Update an existing job invite.
   app.put(
-    '/api/v1/invites/:inviteId',
+    "/api/v1/invites/:inviteId",
     validateInviteUpdateData,
     validateInviteId,
     authenticateUserToken,
@@ -156,7 +201,7 @@ export const initRoutes = app => {
 
   // Delete an existing job invite.
   app.delete(
-    '/api/v1/invites/:inviteId',
+    "/api/v1/invites/:inviteId",
     validateInviteId,
     authenticateUserToken,
     validateAdmin,
@@ -165,11 +210,11 @@ export const initRoutes = app => {
   );
 
   // Get all comments for a given Invite.
-  app.get('/api/v1/comments/:inviteId', validateInviteId, getComments);
+  app.get("/api/v1/comments/:inviteId", validateInviteId, getComments);
 
   // Post a comment on a specific Invite.
   app.post(
-    '/api/v1/comments/:inviteId',
+    "/api/v1/comments/:inviteId",
     validateCommentData,
     validateInviteId,
     authenticateUserToken,
@@ -178,27 +223,31 @@ export const initRoutes = app => {
   );
 
   // New Upvote stuff
-  app.get('/api/v1/invites/:inviteId/votes',
+  app.get(
+    "/api/v1/invites/:inviteId/votes",
     validateInviteId,
     validateInvite,
     fetchVoteCount
   );
 
-  app.patch('/api/v1/invites/:inviteId/upvote',
+  app.patch(
+    "/api/v1/invites/:inviteId/upvote",
     authenticateUserToken,
     validateInviteId,
     validateInvite,
     upvoteInvite
   );
 
-  app.patch('/api/v1/invites/:inviteId/downvote',
+  app.patch(
+    "/api/v1/invites/:inviteId/downvote",
     authenticateUserToken,
     validateInviteId,
     validateInvite,
     downvoteInvite
   );
 
-  app.delete('/api/v1/invites/:inviteId/vote',
+  app.delete(
+    "/api/v1/invites/:inviteId/vote",
     authenticateUserToken,
     validateInviteId,
     validateInvite,
@@ -206,12 +255,18 @@ export const initRoutes = app => {
   );
 
   // Get the number of users, invites and comments in the database.
-  app.get('/api/v1/metrics', getMetrics);
+  app.get("/api/v1/metrics", getMetrics);
 
   // Get all comments for a given Invite.
-  app.get('/api/v1/notifications/:userId', validateUserId, getNotifications);
-  app.post('/api/v1/notifications', validateNotificationData, createNotification);
+  app.get("/api/v1/notifications/:userId", validateUserId, getNotifications);
+  app.post(
+    "/api/v1/notifications",
+    validateNotificationData,
+    createNotification
+  );
 
   // Fallback case for unknown URIs.
-  app.all('*', (req, res) => res.status(404).json({ message: 'Route Not Found' }));
+  app.all("*", (req, res) =>
+    res.status(404).json({ message: "Route Not Found" })
+  );
 };
