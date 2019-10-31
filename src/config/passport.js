a0d2f1d1a6a86b1c06dfcd0bda01f857633059a8
Oluwaseyi Ogunjuyigbe <seyiogunjuyigbe@gmail.com>
2019-10-31 13:57:09 +0100

    Update passport.js

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,20 +1,160 @@
 import passport from 'passport';
-let TwitterStrategy  = require('passport-twitter').Strategy;
-import { updateOneUser, findUsers } from '../services/userServices'
-import User from "../models/user"
-module.exports = ()=> {
+import TwitterStrategy from 'passport-twitter';
+import GoogleStrategy from 'passport-google-oauth20';
+import FacebookStrategy from 'passport-facebook';
+import randomstring from 'randomstring';
+import Model from '../models';
+import { passwordHash } from '../helpers/hash';
+import {
+  findSingleUser, createUser, updateOneUser
+} from '../services/userServices';
+import { twitterConfig, googleConfig, facebookConfig } from './socialAuth';
 
-    passport.serializeUser(function(user, done) {
-        done(null, user.id);
-    });
+const TwitterAuthStrategy = TwitterStrategy.Strategy;
 
-    // used to deserialize the user
-    passport.deserializeUser(function(id, done) {
-        User.findUsers(id, function(err, user) {
-            done(err, user);
+const FacebookAuthStrategy = FacebookStrategy.Strategy;
+
+const { User } = Model;
+
+// module.exports = () => {
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// used to deserialize the user
+passport.deserializeUser((id, done) => {
+  User.findUsers(id, (err, user) => {
+    done(err, user);
+  });
+});
+
+// Google Authentication
+passport.use(new TwitterAuthStrategy(twitterConfig,
+  async (accessToken, refreshToken, profile, done) => {
+    console.log(profile);
+    // try {
+    //   const {
+    //     sub, name, picture, email
+    //   } = profile._json;
+    //   const username = name.replace(/\s/g, '').toLowerCase(); // Create username by joining name string
+    //   const user = await findSingleUser({ email });
+    //   const password = await passwordHash(accessToken);
+    //   const usernameTaken = await findSingleUser({ username });
+    //   // Create new user if not exists
+    //   if (!user) {
+    //     const newData = {};
+    //     newData.name = name;
+    //     newData.googleId = sub;
+    //     newData.profileImage = picture;
+    //     newData.email = email;
+    //     newData.username = username;
+    //     newData.password = password;
+    //     if (usernameTaken) {
+    //       newData.username = username + randomstring.generate({ length: 7, charset: 'numeric' });
+    //     } else {
+    //       newData.username = username;
+    //     }
+    //     const newUser = await createUser(newData);
+    //     return done(null, newUser.user);
+    //   }
+    //   // Add user's googleId if it doesn't exist already
+    //   if (user && !user.googleId) {
+    //     const { userId } = user;
+    //     const updatedUser = await updateOneUser({ googleId: sub }, { userId }).catch(e => {
+    //       throw e;
+    //     });
+    //     return done(null, updatedUser);
+    //   }
+    //   // Return user if user exists
+    //   return done(null, user);
+    // } catch (err) {
+    //   return done(err);
+    // }
+  }));
+
+// Google Authentication
+passport.use(new GoogleStrategy(googleConfig,
+  async (accessToken, refreshToken, profile, done) => {
+    try {
+      const {
+        sub, name, picture, email
+      } = profile._json;
+      const username = name.replace(/\s/g, '').toLowerCase(); // Create username by joining name string
+      const user = await findSingleUser({ email });
+      const password = await passwordHash(accessToken);
+      const usernameTaken = await findSingleUser({ username });
+      // Create new user if not exists
+      if (!user) {
+        const newData = {};
+        newData.name = name;
+        newData.googleId = sub;
+        newData.profileImage = picture;
+        newData.email = email;
+        newData.username = username;
+        newData.password = password;
+        if (usernameTaken) {
+          newData.username = username + randomstring.generate({ length: 7, charset: 'numeric' });
+        } else {
+          newData.username = username;
+        }
+        const newUser = await createUser(newData);
+        return done(null, newUser.user);
+      }
+      // Add user's googleId if it doesn't exist already
+      if (user && !user.googleId) {
+        const { userId } = user;
+        const updatedUser = await updateOneUser({ googleId: sub }, { userId }).catch(e => {
+          throw e;
         });
-    });
+        return done(null, updatedUser);
+      }
+      // Return user if user exists
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }));
 
-
-};
-
+// Facebook Authentication
+passport.use(new FacebookAuthStrategy(facebookConfig,
+  async (accessToken, refreshToken, profile, done) => {
+    try {
+      const {
+        id, displayName, photos, emails
+      } = profile;
+      const email = emails[0].value;
+      const username = displayName.replace(/\s/g, '').toLowerCase(); // Create username by joining name string
+      const user = await findSingleUser({ email });
+      const password = await passwordHash(accessToken);
+      const usernameTaken = await findSingleUser({ username });
+      // Create new user if not exists
+      if (!user) {
+        const newData = {};
+        newData.name = displayName;
+        newData.facebookId = id;
+        newData.profileImage = photos[0].value;
+        newData.email = email;
+        newData.username = username;
+        newData.password = password;
+        if (usernameTaken) {
+          newData.username = username + randomstring.generate({ length: 7, charset: 'numeric' });
+        } else {
+          newData.username = username;
+        }
+        const newUser = await createUser(newData);
+        return done(null, newUser.user);
+      }
+      // Add user's googleId if it doesn't exist already
+      if (user && !user.googleId) {
+        const { userId } = user;
+        const updatedUser = await updateOneUser({ facebookId: id }, { userId }).catch(e => {
+          throw e;
+        });
+        return done(null, updatedUser);
+      }
+      // Return user if user exists
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }));
