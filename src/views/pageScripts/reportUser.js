8e24c93522fe21ea9843c54c33cc4a85b5a45f76
Akinlade Feranmi <feranmiakinlade@gmail.com>
2019-11-03 05:34:13 +0100

    Add report user link to profile page

---
+++ src/views/pageScripts/reportUser.js
@@ -0,0 +1,5 @@
+const formData = {
+  offender: document.querySelector('.report-form__username'),
+  offence: document.querySelector('.report-form__offence'),
+  details: document.querySelector('.report-form__details')
+}
