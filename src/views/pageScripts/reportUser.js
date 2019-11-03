98942f844e0923cb5b85ceffd08ed2e79015d030
Akinlade Feranmi <feranmiakinlade@gmail.com>
2019-11-03 07:32:13 +0100

    Hide report user link on profile page until feature is fully implemented

--- src/views/pageScripts/reportUser.js
+++ src/views/pageScripts/reportUser.js
@@ -23,7 +23,7 @@ window.onload = (event)=> {
     togglePreloader('block');
     setTimeout(() => {
       showSuccessDialog();
-    }, 3000);
+    }, 2000);
 
     /*Work In Progress. Dummy implementation above will be removed once completed.*/
     return;
