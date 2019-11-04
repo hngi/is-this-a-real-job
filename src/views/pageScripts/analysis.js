fd38c522f3d8fa588548a164ec11896b2d0e5c33
shonubijerry <shonubijerry@gmail.com>
2019-11-04 09:40:35 +0100

    improve analysis feature

--- src/views/pageScripts/analysis.js
+++ src/views/pageScripts/analysis.js
@@ -2,14 +2,16 @@
 /* eslint-disable no-console */
 /* eslint-disable no-undef */
 
+
 function togglePreloader(state) {
   const preloader = document.querySelector('#cover');
   preloader.style.display = state;
 }
 
 function getAnalysis(btnType) {
-  $('#analysisModal').modal({ show: false });
-  const notification = document.querySelector('.notification');
+  $('#analysisModal').modal({
+    show: false
+  });
   const apiEndpoint = document.querySelector('#api_endpoint').innerHTML;
   const analysisText = document.querySelector('#analysis_text');
   togglePreloader('block');
@@ -25,19 +27,23 @@ function getAnalysis(btnType) {
       mode: 'no-cors'
     };
   } else if (btnType === 3) {
-    uri = 'https://itarj-cors.appspot.com/https://isthisarealjob-api.appspot.com';
+    // uri = 'https://itarj-cors.appspot.com/https://isthisarealjob-api.appspot.com';
     const company = document.querySelector('#company');
     const location = document.querySelector('#location');
     const jobDetails = document.querySelector('#jobDetails');
-    const data = { company, address: location, invite: jobDetails };
+    const data = {
+      comp: company,
+      address: location,
+      invite: jobDetails
+    };
     options = {
       method: 'POST',
-      body: JSON.stringify({
-        company: 'NNPC',
-        address: 'no. 21 adesanya aguda, surulere',
-        invite: 'You are welcom'
-      }),
-      headers: new Headers({ 'Content-Type': 'application/json', Origin: 'x-requested-with' })
+      hostname: 'itarj-cors.appspot.com',
+      path: '/https://isthisarealjob-api.appspot.com',
+      headers: {
+        Origin: 'x-requested-with',
+        'Content-Type': 'x-requested-with'
+      }
     };
   } else {
     formData.append('title', analysisText.value);
@@ -65,7 +71,9 @@ function getAnalysis(btnType) {
 }
 
 function selectAnalysis(item) {
-  $('#analysisModal').modal({ show: false });
+  $('#analysisModal').modal({
+    show: false
+  });
   const x = item.selectedIndex;
   const invite = [];
   const inputForm = document.querySelector('#analysis_text');
@@ -107,3 +115,110 @@ function selectAnalysis(item) {
     }
   }
 }
+
+
+// function togglePreloader(state) {
+//   const preloader = document.querySelector('#cover');
+//   preloader.style.display = state;
+// }
+
+// function getAnalysis(btnType) {
+//   $('#analysisModal').modal({ show: false });
+//   const notification = document.querySelector('.notification');
+//   const apiEndpoint = document.querySelector('#api_endpoint').innerHTML;
+//   const analysisText = document.querySelector('#analysis_text');
+//   togglePreloader('block');
+
+//   let uri = '';
+//   const formData = new FormData();
+//   let options = {};
+
+//   if (btnType === 2) {
+//     uri = apiEndpoint;
+//     options = {
+//       method: 'GET',
+//       mode: 'no-cors'
+//     };
+//   } else if (btnType === 3) {
+//     uri = 'https://itarj-cors.appspot.com/https://isthisarealjob-api.appspot.com';
+//     const company = document.querySelector('#company');
+//     const location = document.querySelector('#location');
+//     const jobDetails = document.querySelector('#jobDetails');
+//     const data = { company, address: location, invite: jobDetails };
+//     options = {
+//       method: 'POST',
+//       body: JSON.stringify({
+//         company: 'NNPC',
+//         address: 'no. 21 adesanya aguda, surulere',
+//         invite: 'You are welcom'
+//       }),
+//       headers: new Headers({ 'Content-Type': 'application/json', Origin: 'x-requested-with' })
+//     };
+//   } else {
+//     formData.append('title', analysisText.value);
+//     options = {
+//       method: 'POST',
+//       body: formData,
+//       mode: 'no-cors'
+//     };
+//   }
+
+//   fetch(uri, options)
+//     .then(res => {
+//       togglePreloader('none');
+//       console.log(res);
+//       const analysisModal = document.querySelector('#result');
+//       analysisModal.innerHTML = JSON.stringify(res);
+//       $('#analysisModal').modal('show');
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       const analysisModal = document.querySelector('#result');
+//       analysisModal.innerHTML = JSON.stringify(err);
+//       $('#analysisModal').modal('show');
+//     });
+// }
+
+// function selectAnalysis(item) {
+//   $('#analysisModal').modal({ show: false });
+//   const x = item.selectedIndex;
+//   const invite = [];
+//   const inputForm = document.querySelector('#analysis_text');
+//   const inviteData = document.querySelectorAll('.invite-data');
+//   const apiEndpoint = document.querySelector('#api_endpoint');
+//   inviteData.forEach((data) => {
+//     invite.push(data.getAttribute('data-info'));
+//   });
+//   switch (x) {
+//     case 1: {
+//       inputForm.value = invite[3];
+//       apiEndpoint.innerHTML = `http://34.66.66.215/?email=${invite[3]}`;
+//       break;
+//     }
+//     case 2: {
+//       inputForm.value = invite[3];
+//       apiEndpoint.innerHTML = `http://34.66.66.215/error_counter?msg_body=${invite[3]}`;
+//       break;
+//     }
+//     case 3: {
+//       inputForm.value = invite[2];
+//       apiEndpoint.innerHTML = `http://34.66.66.215/check_address?address=${invite[2]}`;
+//       break;
+//     }
+//     case 4: {
+//       inputForm.value = invite[1];
+//       apiEndpoint.innerHTML = `http://34.66.66.215/check_name?company_name=${invite[1]}`;
+//       break;
+//     }
+//     case 5: {
+//       inputForm.value = invite[1];
+//       apiEndpoint.innerHTML = `http://34.66.66.215/nairaland_sentimental_analysis?searchTerm=${invite[1]}`;
+//       break;
+//     }
+//     default: {
+//       inputForm.value = invite[1];
+//       apiEndpoint.innerHTML = `http://34.66.66.215/match_name_and_address?name=&address=${invite[1]}`;
+//       break;
+//     }
+//   }
+// }
